bf79e4e5d5abf92ca925708ca33a128549099b5c
Jose Naranjo <naranjja@gmail.com>
2017-05-15 01:16:02 -0500

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # python27-selenium-tor-windows
 Python 2.7 Selenium script for using The Onion Router nodes and relays in Windows (tested on Windows 10 64-bit).
+
+## Requirements
+* [Python 2.7](https://www.python.org/downloads/)
+* [Tor Browser Bundle](https://www.torproject.org/download/download-easy.html.en)
+
+## Instructions
+1. Remember path to Tor Browser Bundle installation
+2. Run `pip install -r requirements.txt` inside project folder
+3. Open `router.py` and modify the path to binary
+4. Insert Selenium code within `# do something`
+5. Open an instance of Tor Browser manually using `Start Tor Browser.lnk`
+5. Run script!
+
+## Custom settings
+* The `setup_tor` function contains the profile preferences for the Tor session. Modify these settings if necessary. They are implemented to ensure anonymity and default configuration changes.
+* However, if SOCKS port has changed for some reason (typically to `9151`), here is where you would modify the port.
+* If some webpages are displaying improperly, this may be due to JavaScript being toggled off. Simply change `javascript.enabled` to `True`. Note that this may compromise anonymity.
---
+++ router.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.common.exceptions import NoSuchElementException
+
+def setup_tor(binary):
+    global driver
+    driver = webdriver.Firefox(firefox_binary=binary)
+    driver.profile.set_preference('network.proxy.type', 1)
+    driver.profile.set_preference('network.proxy.socks', '127.0.0.1')
+    driver.profile.set_preference('network.proxy.socks_port', 9051)
+    driver.profile.set_preference('network.cookie.lifetimePolicy', 2)
+    driver.profile.set_preference('network.dns.disablePrefetch', True)
+    driver.profile.set_preference('network.http.sendRefererHeader', 0)
+    driver.profile.set_preference('privacy.clearOnShutdown.offlineApps', True)
+    driver.profile.set_preference('privacy.clearOnShutdown.passwords', True)
+    driver.profile.set_preference('privacy.clearOnShutdown.siteSettings', True)
+    driver.profile.set_preference('privacy.sanitize.sanitizeOnShutdown', True)
+    driver.profile.set_preference('signon.rememberSignons', False)
+    driver.profile.set_preference('places.history.enabled', False)
+    driver.profile.set_preference('javascript.enabled', False)
+
+
+def check_tor():
+    try:
+        driver.get('https://check.torproject.org')
+        if 'tor-on' in driver.find_element_by_class_name('onion').get_attribute('src'):
+            print 'Connection successful.'
+            print 'Configuration successful.'
+            return True
+        else:
+            print 'Connection successful.'
+            print 'Configuration error.'
+            driver.quit()
+            return False
+    except NoSuchElementException:
+        print 'Connection error.'
+        driver.quit()
+        return False
+
+
+
+if __name__ == '__main__':
+    binary = FirefoxBinary('path/to/Tor Browser/Browser/firefox.exe')
+
+    setup_tor(binary)
+    check_tor()
+
+    # do something
+
+    driver.quit()
